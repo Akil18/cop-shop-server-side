a466720922f1bf4067dd11eaf0f337a4871b4bb9
Akil Chowdhury <akiluddinchy@gmail.com>
2022-11-24 02:04:14 +0600

    connect to database and add categories api

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
+const port = 5000 || process.env.PORT;
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvyyp05.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//API 
+async function run() {
+    try{
+        const categoriesCollection = client.db("copshop").collection("categories");
+
+        //Get All Categories
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        });
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
